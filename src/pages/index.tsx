a633eec4b342b01547317ce3db10b9d9626e23fb
Mags <dev@mags.page>
2021-03-15 03:24:51 -0400

    bootstrap app

---
+++ src/pages/index.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+import SEO from '../components/seo';
+
+const IndexPage = () => (
+  <>
+    <SEO title="Home" />
+    <h1>Hello, World!</h1>
+  </>
+);
+
+export default IndexPage;
